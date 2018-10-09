e480eda3e7325ccba7bce54dc2b6ed7403deff55
Sponja- <tomas.spognardi@gmail.com>
2018-10-09 19:09:15 -0300

    Worked on GUI

--- GUI/main.py
+++ GUI/main.py
@@ -2,11 +2,23 @@ import kivy
 kivy.require("1.10.1")
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from editable_label import EditableLabel
+
+
+class Element(BoxLayout):
+	pass
+
+class TextElement(Element):
+	pass
+
+class Formula(Widget):
+	pass
 
 class FCAD(App):
 	def build(self):
-		return Label(text="FCAD")
+		return Formula()
 
 if __name__ == '__main__':
 	FCAD().run()
